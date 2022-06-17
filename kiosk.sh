d4e39795a85b129f7d7bfcf78b8dcbeb7d1983ec
jeffpm <jeffpm@gmail.com>
2022-06-17 12:27:03 -0400

    Update kiosk.sh

--- kiosk.sh
+++ kiosk.sh
@@ -1,2 +1,21 @@
+#!/bin/bash
+
+xset s noblank
+xset s off
+xset -dpms
+
+unclutter -idle 0.5 -root &
+
+sed -i 's/"exited_cleanly":false/"exited_cleanly":true/' /home/pi/.config/chromium/Default/Preferences
+sed -i 's/"exit_type":"Crashed"/"exit_type":"Normal"/' /home/pi/.config/chromium/Default/Preferences
+
 /usr/bin/chromium-browser --kiosk fiele://cotu-pinball.jpg https://insider.sternpinball.com/kiosk/ags-usWmN-xkOX
-#/usr/bin/chromium-browser --kiosk https://insider.sternpinball.com/kiosk/aTxvx-KwtN-zqNph
+
+# cotu leaderboard - https://insider.sternpinball.com/kiosk/aTxvx-KwtN-zqNph
+
+while true; do
+      xdotool keydown ctrl+Tab; xdotool keyup ctrl+Tab;
+      sleep 30
+      xdotool keydown ctrl+Tab; xdotool keyup ctrl+Tab;
+      sleep 60
+done
