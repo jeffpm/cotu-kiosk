8f650ca6225e8f5bc8101ebf30e4914d276b8837
jeffpm <jeffpm@gmail.com>
2022-06-17 12:22:51 -0400

    Update kiosk.sh

--- kiosk.sh
+++ kiosk.sh
@@ -1,2 +1,2 @@
-/usr/bin/chromium-browser --kiosk https://insider.sternpinball.com/kiosk/ags-usWmN-xkOX
+/usr/bin/chromium-browser --kiosk fiele://cotu-pinball.jpg https://insider.sternpinball.com/kiosk/ags-usWmN-xkOX
 #/usr/bin/chromium-browser --kiosk https://insider.sternpinball.com/kiosk/aTxvx-KwtN-zqNph
