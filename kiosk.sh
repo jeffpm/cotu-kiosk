c14a4e809f143edecdbf0eb5bde63e671eb19cfa
jeffpm <jeffpm@gmail.com>
2022-07-07 18:55:47 -0400

    Update kiosk.sh

--- kiosk.sh
+++ kiosk.sh
@@ -9,7 +9,7 @@ unclutter -idle 0.5 -root &
 sed -i 's/"exited_cleanly":false/"exited_cleanly":true/' ~/.config/chromium/Default/Preferences
 sed -i 's/"exit_type":"Crashed"/"exit_type":"Normal"/' ~/.config/chromium/Default/Preferences
 
-$PAGES = "file:///home/dan/cotu-kiosk/cotu-pinball.jpg https://insider.sternpinball.com/kiosk/fpdgr-PeKmG-mQW"
+PAGES = "file:///home/dan/cotu-kiosk/cotu-pinball.jpg https://insider.sternpinball.com/kiosk/fpdgr-PeKmG-mQW"
 
 /usr/bin/chromium-browser --kiosk --enable-kiosk-mode --noerrdialogs --disable-infobars --disable-ipv6 --allow-file-access-from-files --disable-java --disable-restore-session-state --disable-sync --disable-translate --disk-cache-size=1 --media-cache-size=1 $PAGES &
 # /usr/bin/chromium-browser --kiosk --enable-kiosk-mode --noerrdialogs --disable-infobars --disable-ipv6 --allow-file-access-from-files --disable-java --disable-restore-session-state --disable-sync --disable-translate --disk-cache-size=1 --media-cache-size=1 file:///home/jeffpm/cotu-kiosk/cotu-pinball.jpg https://insider.sternpinball.com/kiosk/aTxvx-KwtN-zqNph file:///home/jeffpm/cotu-kiosk/mando.jpg &
