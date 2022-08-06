964a1fd8d9be0609a55dc57950f623bfae887861
jeffpm <jeffpm@gmail.com>
2022-08-06 08:47:03 -0400

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,9 +1,6 @@
 #!/bin/bash
 set -e
 
-sudo apt purge wolfram-engine scratch scratch2 nuscratch sonic-pi idle3 -y
-sudo apt purge smartsim java-common minecraft-pi libreoffice* -y
-
 sudo apt clean
 sudo apt autoremove -y
 
