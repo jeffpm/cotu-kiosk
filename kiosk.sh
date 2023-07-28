1c597dc0535a7f467c3233c6c9a9c357b355e72d
FlexBT <109229702+FlexBT@users.noreply.github.com>
2023-07-28 12:57:49 -0400

    Update kiosk.sh
    trying getactivewindow command to try to identify what window xdotool is being used on that isn't chromium

--- kiosk.sh
+++ kiosk.sh
@@ -15,7 +15,7 @@ pages="https://insider.sternpinball.com/kiosk/ktim-MiwwU-MiM file:///home/dan/co
 # /usr/bin/chromium-browser --kiosk --enable-kiosk-mode --noerrdialogs --disable-infobars --disable-ipv6 --allow-file-access-from-files --disable-java --disable-restore-session-state --disable-sync --disable-translate --disk-cache-size=1 --media-cache-size=1 file:///home/jeffpm/cotu-kiosk/cotu-pinball.jpg https://insider.sternpinball.com/kiosk/aTxvx-KwtN-zqNph file:///home/jeffpm/cotu-kiosk/mando.jpg &
 
 while true; do
-      xdotool search --class chromium windowactivate
       xdotool keydown ctrl+Tab; xdotool keyup ctrl+Tab;
       sleep 15
+      xdotool getactivewindow
 done
